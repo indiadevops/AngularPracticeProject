30060c972bce8fa5ea51499fb0b74f0b8d305cd5
Hemant Nagpal <hemant.nagpal110@gmail.com>
2019-04-07 19:46:44 +0530

    This is first commit

---
+++ WebGYM/WebGYM/CommandHandlers/User/NewUserHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using WebGYM.Command.User;
+using WebGYM.Common;
+using WebGYM.Interface;
+using WebGYM.Models;
+
+namespace WebGYM.CommandHandlers.User
+{
+    public class NewUserHandler : IRequestHandler<NewUser, Users>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUsers _users;
+        public NewUserHandler(IMapper mapper,IUsers users)
+        {
+            _mapper = mapper;
+            _users = users;
+        }
+        public Task<Users> Handle(NewUser request, CancellationToken cancellationToken)
+        {
+            var tempUsers = AutoMapper.Mapper.Map<Users>(request);
+            tempUsers.CreatedDate = DateTime.Now;
+            tempUsers.Createdby = 1;
+            tempUsers.Password = EncryptionLibrary.EncryptText(request.Password);
+            _users.InsertUsers(tempUsers);
+            return Task.FromResult(tempUsers);
+        }
+    }
+}
